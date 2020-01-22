b254c3a7cab622e96caff3824a9f3543d397c212
AkramYamin <ak.5@yahoo.com>
2020-01-22 21:32:46 +0200

    Added bootstrap and Jobs are showing up

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -11,13 +11,15 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('Blog/', include('Blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+import Jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Jobs.views.home, name='jobs_home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
